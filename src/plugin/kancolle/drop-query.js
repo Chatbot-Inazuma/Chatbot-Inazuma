f6fce596f3c8876f5eda54371ba163bf0066aabd
MomoCow <momocow.me@gmail.com>
2017-08-10 22:05:37 +0800

    refine query algorithm

--- src/plugin/kancolle/drop-query.js
+++ src/plugin/kancolle/drop-query.js
@@ -28,8 +28,60 @@ function readCSONFile(cson){
   }
 }
 
+function output(group, query_result){
+  global.bot.output("message.cq-bot.group", {
+    src: {
+      id: INFO.id,
+      mod: path.basename(__filename, ".js")
+    },
+    gid: group,
+    msg: query_result
+  });
+}
+
+function processOutput(fromGroup, test_name, err, res){
+  if(err || !(res = parsePoiStatJSON(res))){
+    logger.error(`Cannot find drop statistics for Ship '${test_name}'`);
+    output(fromGroup, `電醬沒有在Poi資料庫找到${ch.s2t(test_name)}的資料[CQ:face,id=9]`);
+    return; //not found
+  }
+
+  let query_result = `${ch.s2t(test_name)}:\n`;
+  if(res.type == "interested"){
+    delete res.type;
+
+    for(let map_name of _.keys(res)){
+      let kaiiki_stage = map_name.split("-");
+      let map_readable = SHIP_EXDATA.mapWikiData[parseInt(`${kaiiki_stage[0]}${kaiiki_stage[1]}`)];
+      map_readable = map_readable.replace(/^20([0-9]{2})年(.)季活动\/(E-\d)$/, "$1$2$3");
+      query_result += ` - 在${map_readable}的掉落率為${res[map_name].rate}%\n`;
+      query_result += `   母數為${res[map_name].totalCount}件, `;
+      query_result += `S勝${res[map_name].rankCount[0]}件, A勝${res[map_name].rankCount[1]}件, B勝${res[map_name].rankCount[2]}件\n`;
+      query_result += `   提督等級分布在Lv.${res[map_name].hqLv[0]}~Lv.${res[map_name].hqLv[1]}\n`;
+    }
+  }
+  else if(res.type == "highest" && !_.isEmpty(res.name)){
+    let kaiiki_stage = res.name.split("-");console.log(res.name)
+    let map_readable = SHIP_EXDATA.mapWikiData[parseInt(`${kaiiki_stage[0]}${kaiiki_stage[1]}`)];
+    map_readable = map_readable.replace(/^20([0-9]{2})年(.)季活动\/(E-\d)$/, "$1$2$3");
+    query_result += ` - 在一般海域中以${map_readable}的掉落率${res.stat.rate}%最高\n`;
+    query_result += `   母數為${res.stat.totalCount}件, `;
+    query_result += `S勝${res.stat.rankCount[0]}件, A勝${res.stat.rankCount[1]}件, B勝${res.stat.rankCount[2]}件\n`;
+    query_result += `   提督等級分布在Lv.${res.stat.hqLv[0]}~Lv.${res.stat.hqLv[1]}\n`;
+  }
+  else{
+    query_result = `電醬沒找到${test_name}在本季活動的資料\n在一般海域也沒有掉落 [CQ:face,id=107]`;
+  }
+
+  if(_.last(query_result) == "\n"){
+    query_result = query_result.substr(0, query_result.length - 1);
+  }
+
+  output(fromGroup, query_result);
+}
+
 function parsePoiStatJSON(poi_stat){
-  if(!CONFIG) return undefined;
+  if(!CONFIG || !poi_stat || !poi_stat.data) return undefined;
 
   let interested = {};
   let highest = {
@@ -40,10 +92,12 @@ function parsePoiStatJSON(poi_stat){
 
   for(let map_name of _.keys(poi_stat.data)){
     let kaiiki = map_name.split("-")[0];
+    // interested
     if(_.indexOf(CONFIG.interested_maps, kaiiki) >= 0){
       interested[map_name] = {};
       _.assign(interested[map_name], poi_stat.data[map_name]);
     }
+    // highest
     if((kaiiki <= 6 && kaiiki > 0) && (_.isEmpty(highest.name) || poi_stat.data[map_name].rate > highest.stat.rate)){
       highest.name = map_name;
       _.assign(highest.stat, poi_stat.data[map_name]);
@@ -77,59 +131,20 @@ module.exports = {
           for(let name_length = candidate.length; name_length > 0; name_length--){
             for(let start_idx = 0; start_idx + name_length <= candidate.length; start_idx++){
               let test_name = candidate.substr(start_idx, name_length), pid = -1;
-              if((pid = kcdata.getPoiIDByNameExdata(test_name)) >= 0){ //found
+              if((pid = kcdata.getPoiIDByName(test_name)) >= 0){ //found
                 logger.info(`Ship named ${test_name} is found!`);
                 getJSON(`https://db.kcwiki.org/drop/ship/${pid}/SAB.json`, function(err, res){
-                  if(err){
-                    logger.error(`Cannot find drop statistics for Ship '${test_name}'`);
-                    return; //not found
-                  }
-
-                  res = parsePoiStatJSON(res);
-
-                  let query_result = `${ch.s2t(test_name)}:\n`;
-                  if(res.type == "interested"){
-                    delete res.type;
-
-                    for(let map_name of _.keys(res)){
-                      let kaiiki_stage = map_name.split("-");
-                      let map_readable = SHIP_EXDATA.mapWikiData[parseInt(`${kaiiki_stage[0]}${kaiiki_stage[1]}`)];
-                      map_readable = map_readable.replace(/^20([0-9]{2})年(.)季活动\/(E-\d)$/, "$1$2$3");
-                      query_result += ` - 在${map_readable}的掉落率為${res[map_name].rate}%\n`;
-                      query_result += `   母數為${res[map_name].totalCount}件, `;
-                      query_result += `S勝${res[map_name].rankCount[0]}件, A勝${res[map_name].rankCount[1]}件, B勝${res[map_name].rankCount[2]}件\n`;
-                      query_result += `   提督等級分布在Lv.${res[map_name].hqLv[0]}~Lv.${res[map_name].hqLv[1]}\n`;
-                    }
-                  }
-                  else if(res.type == "highest" && !_.isEmpty(res.name)){
-                    let kaiiki_stage = res.name.split("-");console.log(res.name)
-                    let map_readable = SHIP_EXDATA.mapWikiData[parseInt(`${kaiiki_stage[0]}${kaiiki_stage[1]}`)];
-                    map_readable = map_readable.replace(/^20([0-9]{2})年(.)季活动\/(E-\d)$/, "$1$2$3");
-                    query_result += ` - 在一般海域中以${map_readable}的掉落率${res.stat.rate}%最高\n`;
-                    query_result += `   母數為${res.stat.totalCount}件, `;
-                    query_result += `S勝${res.stat.rankCount[0]}件, A勝${res.stat.rankCount[1]}件, B勝${res.stat.rankCount[2]}件\n`;
-                    query_result += `   提督等級分布在Lv.${res.stat.hqLv[0]}~Lv.${res.stat.hqLv[1]}\n`;
-                  }
-                  else{
-                    query_result = `電醬沒找到${ch.s2t(test_name)}的資料[CQ:face,id=9]`;
-                  }
-
-                  if(_.last(query_result) == "\n"){
-                    query_result = query_result.substr(0, query_result.length - 1);
-                  }
-
-                  global.bot.output("message.cq-bot.group", {
-                    src: {
-                      id: INFO.id,
-                      mod: path.basename(__filename, ".js")
-                    },
-                    gid: bundle.fromGroup,
-              			msg: query_result
-                  });
+                  processOutput(bundle.fromGroup, test_name, err, res);
                 });
                 return;
               }
-
+              else if((pid = kcdata.getPoiIDByNameExdata(test_name)) >= 0){
+                logger.info(`Ship named ${test_name} is found!`);
+                getJSON(`https://db.kcwiki.org/drop/ship/${pid}/SAB.json`, function(err, res){
+                  processOutput(bundle.fromGroup, test_name, err, res);
+                });
+                return;
+              }
               // not found
               logger.error(`Cannot find a ship named '${test_name}'`);
             }
